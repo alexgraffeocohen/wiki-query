c3227ab4a7b36a649d42a88f8b3d2f6a66ad09e6
Alex Wilkinson <alex.wilkinson@me.com>
2013-11-22 14:38:43 -0500

    Removed write-in-place hack, now writes out to normal.csv. Added freebase call.

--- wiki_query.rb
+++ wiki_query.rb
@@ -4,98 +4,95 @@ require 'uri'
 
 # VARIABLES
 
-base_url = 'http://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&titles=Barack%20obama&redirects&format=json'
+base_wiki_url = 'http://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&titles=Barack%20obama&redirects&format=json'
+base_freebase_url = 'https://www.googleapis.com/freebase/v1/mqlread?query={"mid":null,"id":null,"key":{"namespace":"/wikipedia/en_id","value":"wikikey","limit":1}}'
 i = 0
 no_page_iterator = 0
 no_page_count = 0
 multi_page_iterator = 0
 multi_page_count = 0
 loop_count = 0
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 # PROGRAM
 
-CSV.foreach('test.csv', { encoding: "UTF-8"}) { |csv|
-
-	# READ CSV, CALL API
-
+CSV.foreach('test.csv') { |csv|
 	row = csv
-	break if row[0] == ";" #END OF CSV FILE
-	start_time = Time.now
-	api_call = HTTParty.get(URI.encode(base_url.gsub(/Barack%20obama/, row[3].to_s)))
-	end_time = Time.now
-	puts "Made query"
-	time_interval = end_time - start_time
-	api_call_pages = api_call["query"]["pages"]
 
-	# IF THERE ARE NO PAGES
+	#WIKIPEDIA API
 
-	if api_call_pages.keys == ["-1"]
+	wiki_call = HTTParty.get(URI.encode(base_wiki_url.gsub(/Barack%20obama/, row[3].to_s)))
+	puts "Made Wikipedia query for #{row[3]}..."
+	wiki_call_pages = wiki_call["query"]["pages"]
+
+	if wiki_call_pages.keys == ["-1"]
 		if no_page_iterator < 1
 			CSV.open('missing.csv', mode = "w") { |missing|
 				missing << row
 			}
 			no_page_iterator += 1
 			no_page_count += 1
+			puts "No entry found! Logged to missing.csv."
 			next
 		else
 			CSV.open('missing.csv', mode = "a+") { |missing|
 				missing << row
 			}
 			no_page_count += 1
+			puts "No entry found! Logged to missing.csv."
 			next
 		end
 	end
 
-	# IF THERE ARE MULTIPLE PAGES
-
-	if api_call_pages.keys[1] != nil
+	if wiki_call_pages.keys[1] != nil
 		if multi_page_iterator < 1
-			CSV.open('multiple.csv', mode = "w") { |missing|
-				missing << row
+			CSV.open('multiple.csv', mode = "w") { |multiple|
+				multiple << row
 			}
 			multi_page_iterator += 1
 			multi_page_count += 1
+			puts "Multiple entries found! Logged to multiple.csv."
 			next
 		else
-			CSV.open('multiple.csv', mode = "a+") { |missing|
-				missing << row
+			CSV.open('multiple.csv', mode = "a+") { |multiple|
+				multiple << row
 			}
 			multi_page_count += 1
+			puts "Multiple entries found! Logged to multiple.csv."
 			next
 		end
 	end
 
-	# IF NORMAL
+	row[10] = wiki_call_pages.values.first.values[0]
+	row[11] = wiki_call_pages.values.first.values[9]
+	row[12] = wiki_call_pages.values.first.values[2]
 
-	row[10] = api_call_pages.values.first.values[0]
-	row[11] = api_call_pages.values.first.values[9]
-	row[12] = api_call_pages.values.first.values[2]
-	print row
+	# FREEBASE API
 
-	# WRITE IN-PLACE
+	freebase_call = HTTParty.get(URI.encode(base_freebase_url.gsub(/wikikey/, row[10].to_s)))
+	puts "Made Freebase query for Wiki ID##{row[10]}..."
+
+	row[14] = freebase_call["result"]["id"]
+	row[15] = freebase_call["result"]["mid"]
+
+	# WRITE OUT TO NORMAL.CSV
 
 	if i < 1
-		CSV.open('test.csv', mode = "w") { |file|
+		CSV.open('normal.csv', mode = "w") { |file|
 			file << row
 			i += 1
 		}
 	else
-		CSV.open('test.csv', mode = "a+") { |file|
+		CSV.open('normal.csv', mode = "a+") { |file|
 			file << row
 		}
 	end
 
-	# SLEEP
-
-	if time_interval > 1
-		sleep_time = 1
-	else
-		sleep_time = 1 - (time_interval)
-	end
-	puts "Sleep Time:\t#{sleep_time}\n"
-	sleep sleep_time
+	puts "Sleeping..."
+	sleep 1
 	loop_count += 1
+	puts "-----------"
 }
 
-puts "Done! Completed #{loop_count} queries. #{no_page_count} were logged to missing.csv. #{multi_page_count} were logged to multiple.csv."
+puts "Done! Completed #{loop_count} rows. #{no_page_count} were logged to missing.csv. #{multi_page_count} were logged to multiple.csv."
 
