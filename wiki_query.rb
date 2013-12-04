8d9df6ad0f4dc7cc078ee17c5ea8ddcc2a414f91
Alex Wilkinson <alex.wilkinson@me.com>
2013-12-04 10:43:17 -0500

    changes all file modes to "a+" to simplify code

--- wiki_query.rb
+++ wiki_query.rb
@@ -1,3 +1,4 @@
+require 'csv'
 require 'httparty'
 
 # VARIABLES
@@ -5,15 +6,13 @@ require 'httparty'
 base_wiki_url = 'http://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&titles=Barack%20obama&redirects&format=json'
 base_freebase_url = 'https://www.googleapis.com/freebase/v1/mqlread?query={"mid":null,"id":null,"key":{"namespace":"/wikipedia/en_id","value":"wikikey","limit":1}}'
 i = 0
-no_page_iterator = 0
-no_page_count = 0
-multi_page_iterator = 0
-multi_page_count = 0
-loop_count = 0
-missing_freebase_iterator = 0
-missing_freebase_count = 0
-clean_count = 0
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+loop_count = 0              # counts how many rows have been examined
+no_page_count = 0           # counts how many rows are missing wiki pages
+multi_page_count = 0        # counts how many rows have multiple wiki pages
+missing_freebase_count = 0  # counts how many rows are missing freebase entries
+clean_count = 0             # counts how many rows have no issues
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE    # necessary to force freebase query
 
 # SCRIPT
 
@@ -27,20 +26,7 @@ CSV.foreach('test.csv') { |csv|
 	wiki_call_pages = wiki_call["query"]["pages"]
 
 	if wiki_call_pages.keys == ["-1"]
-		if no_page_iterator < 1
-			CSV.open('missing.csv', mode = "w") { |missing|
-				missing << row
-			}
-			no_page_iterator += 1
-			no_page_count += 1
-			puts "No entry found! Logged to missing.csv."
-			puts "Sleeping..."
-			sleep 1
-			loop_count += 1
-			puts "-----------"
-			next
-		else
-			CSV.open('missing.csv', mode = "a+") { |missing|
+		CSV.open('missing.csv', mode = "a+") { |missing|
 				missing << row
 			}
 			no_page_count += 1
@@ -50,24 +36,10 @@ CSV.foreach('test.csv') { |csv|
 			loop_count += 1
 			puts "-----------"
 			next
-		end
 	end
 
 	if wiki_call_pages.keys[1] != nil
-		if multi_page_iterator < 1
-			CSV.open('multiple.csv', mode = "w") { |multiple|
-				multiple << row
-			}
-			multi_page_iterator += 1
-			multi_page_count += 1
-			puts "Multiple entries found! Logged to multiple.csv."
-			puts "Sleeping..."
-			sleep 1
-			loop_count += 1
-			puts "-----------"
-			next
-		else
-			CSV.open('multiple.csv', mode = "a+") { |multiple|
+		CSV.open('multiple.csv', mode = "a+") { |multiple|
 				multiple << row
 			}
 			multi_page_count += 1
@@ -77,7 +49,6 @@ CSV.foreach('test.csv') { |csv|
 			loop_count += 1
 			puts "-----------"
 			next
-		end
 	end
 
 	row[10] = wiki_call_pages.values.first.values[0]
@@ -90,20 +61,7 @@ CSV.foreach('test.csv') { |csv|
 	puts "Made Freebase query for Wiki ID##{row[10]}..."
 
 	if freebase_call["result"] == nil
-		if missing_freebase_iterator < 1
-			CSV.open('missing_freebase.csv', mode = "w") { |missing|
-				missing << row
-			}
-			missing_freebase_iterator += 1
-			missing_freebase_count += 1
-			puts "Missing freebase entry! Logged to missing_freebase.csv"
-			puts "Sleeping..."
-			sleep 1
-			loop_count += 1
-			puts "-----------"
-			next
-		else
-			CSV.open('missing_freebase.csv', mode = "a+") {|missing|
+		CSV.open('missing_freebase.csv', mode = "a+") {|missing|
 				missing << row
 			}
 			missing_freebase_count +=1
@@ -113,7 +71,6 @@ CSV.foreach('test.csv') { |csv|
 			loop_count += 1
 			puts "-----------"
 			next
-		end
 	end
 
 	row[14] = freebase_call["result"]["id"]
@@ -121,21 +78,14 @@ CSV.foreach('test.csv') { |csv|
 
 	# WRITE OUT TO NORMAL.CSV
 
-	if i < 1
-		CSV.open('normal.csv', mode = "w") { |file|
-			file << row
-			i += 1
-		}
-	else
-		CSV.open('normal.csv', mode = "a+") { |file|
+	CSV.open('normal.csv', mode = "a+") { |file|
 			file << row
 		}
-	end
+	loop_count += 1
+	clean_count += 1
 
 	puts "Sleeping..."
 	sleep 1
-	loop_count += 1
-	clean_count += 1
 	puts "-----------"
 }
 
